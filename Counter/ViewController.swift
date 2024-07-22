411de98b4083f2dbfb2daae6f9c061fa9c21aaf8
fEgor <fegor1818@icloud.com>
2024-07-22 20:23:15 +0200

    commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,19 +1,73 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Admin on 12.07.2024.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+   // @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var counterButtonPlus: UIButton!
+    @IBOutlet weak var counterButtonMinus: UIButton!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var cleanHistoryButton: UIButton!
+    
+    private var counter = 0
+    private var time = DateFormatter()
+    private var formattedDateTime: String = ""
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let currentDate = Date()
+       
+        time.dateStyle = .short
+        time.timeStyle = .short
+        formattedDateTime = time.string(from: currentDate)
+        
+        textView.text = "История изменений: \n"
+        
     }
+    
 
-
+    @IBAction func counterChange2(_ sender: Any) {
+        
+        counter += 1
+        
+        counterLabel.text = "Значение счетчика: \(counter)"
+        
+        textView.text.append("\n\(formattedDateTime) значение изменено на +1")
+    }
+    
+    @IBAction func minusScore(_ sender: Any) {
+        
+        counter -= 1
+       
+        if counter < 0 {
+            counter = 0
+            textView.text.append("\n\(formattedDateTime)  попытка уменьшить значение счётчика ниже 0")
+        }
+        
+        counterLabel.text = "Значение счетчика: \(counter)"
+    
+        
+        textView.text.append("\n\(formattedDateTime) значение изменено на -1")
+    }
+    @IBAction func reserScore(_ sender: Any) {
+        
+        counter = 0
+        
+        counterLabel.text = "Значение счетчика: \(counter)"
+        textView.text.append("\n\(formattedDateTime) значение сброшено")
+        
+    }
+    
+    @IBAction func cleanHistory(_ sender: Any) {
+        
+        textView.text = ""
+    }
+    
+    
 }
 
